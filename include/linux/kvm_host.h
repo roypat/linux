fbe3a7384aa789c32aa1aa034ece2180a7b970ad
James Houghton <jthoughton@google.com>
2025-01-09 20:49:21 +0000

    KVM: x86/mmu: Add support for KVM_MEM_USERFAULT
    Adhering to the requirements of KVM Userfault:
    
    1. Zap all sptes for the memslot when KVM_MEM_USERFAULT is toggled on
       with kvm_arch_flush_shadow_memslot().
    2. Only all PAGE_SIZE sptes when KVM_MEM_USERFAULT is enabled (for both
       normal/GUP memory and guest_memfd memory).
    3. Reconstruct huge mappings when KVM_MEM_USERFAULT is toggled off with
       kvm_mmu_recover_huge_pages(). This is the behavior when dirty logging
       is disabled; remain consistent with it.
    
    With the new logic in kvm_mmu_slot_apply_flags(), I've simplified the
    two dirty-logging-toggle checks into one, and I have dropped the
    WARN_ON() that was there.
    
    Signed-off-by: James Houghton <jthoughton@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2538,7 +2538,8 @@ static inline void kvm_account_pgtable_pages(void *virt, int nr)
 static inline void kvm_prepare_memory_fault_exit(struct kvm_vcpu *vcpu,
 						 gpa_t gpa, gpa_t size,
 						 bool is_write, bool is_exec,
-						 bool is_private)
+						 bool is_private,
+						 bool is_userfault)
 {
 	vcpu->run->exit_reason = KVM_EXIT_MEMORY_FAULT;
 	vcpu->run->memory_fault.gpa = gpa;
@@ -2548,6 +2549,8 @@ static inline void kvm_prepare_memory_fault_exit(struct kvm_vcpu *vcpu,
 	vcpu->run->memory_fault.flags = 0;
 	if (is_private)
 		vcpu->run->memory_fault.flags |= KVM_MEMORY_EXIT_FLAG_PRIVATE;
+	if (is_userfault)
+		vcpu->run->memory_fault.flags |= KVM_MEMORY_EXIT_FLAG_USERFAULT;
 }
 
 static inline bool kvm_memslot_is_gmem_only(const struct kvm_memory_slot *slot)
