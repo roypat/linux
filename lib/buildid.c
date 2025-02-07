5f6171141c067bb8978f7176c89f5e37795baae2
Patrick Roy <roypat@amazon.co.uk>
2025-02-07 11:16:06 +0000

    mm: introduce AS_NO_DIRECT_MAP
    Add AS_NO_DIRECT_MAP for mappings where direct map entries of folios are
    set to not present . Currently, mappings that match this description are
    secretmem mappings (memfd_secret()). Later, some guest_memfd
    configurations will also fall into this category.
    
    Reject this new type of mappings in all locations that currently reject
    secretmem mappings, on the assumption that if secretmem mappings are
    rejected somewhere, it is precisely because of an inability to deal with
    folios without direct map entries, and then make memfd_secret() use
    AS_NO_DIRECT_MAP on its address_space to drop its special
    vma_is_secretmem()/secretmem_mapping() checks.
    
    This drops a optimization in gup_fast_folio_allowed() where
    secretmem_mapping() was only called if CONFIG_SECRETMEM=y. secretmem is
    enabled by default since commit b758fe6df50d ("mm/secretmem: make it on
    by default"), so the secretmem check did not actually end up elided in
    most cases anymore anyway.
    
    Use a new flag instead of overloading AS_INACCESSIBLE (which is already
    set by guest_memfd) because not all guest_memfd mappings will end up
    being direct map removed (e.g. in pKVM setups, parts of guest_memfd that
    can be mapped to userspace should also be GUP-able, and generally not
    have restrictions on who can access it).
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- lib/buildid.c
+++ lib/buildid.c
@@ -65,8 +65,8 @@ static int freader_get_folio(struct freader *r, loff_t file_off)
 
 	freader_put_folio(r);
 
-	/* reject secretmem folios created with memfd_secret() */
-	if (secretmem_mapping(r->file->f_mapping))
+	/* reject folios without direct map entries (e.g. from memfd_secret() or guest_memfd()) */
+	if (mapping_no_direct_map(r->file->f_mapping))
 		return -EFAULT;
 
 	r->folio = filemap_get_folio(r->file->f_mapping, file_off >> PAGE_SHIFT);
