25ded92eed6dc3c12848dfb5ba0903703a818f82
Patrick Roy <roypat@amazon.co.uk>
2025-02-20 11:08:22 +0000

    KVM: selftests: Add guest_memfd based vm_mem_backing_src_types
    Allow selftests to configure their memslots such that userspace_addr is
    set to a MAP_SHARED mapping of the guest_memfd that's associated with
    the memslot. This setup is the configuration for non-CoCo VMs, where all
    guest memory is backed by a guest_memfd whose folios are all marked
    shared, but KVM is still able to access guest memory to provide
    functionality such as MMIO emulation on x86.
    
    Add backing types for normal guest_memfd, as well as direct map removed
    guest_memfd.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tools/testing/selftests/kvm/include/kvm_util.h
+++ tools/testing/selftests/kvm/include/kvm_util.h
@@ -592,6 +592,16 @@ static inline bool is_smt_on(void)
 
 void vm_create_irqchip(struct kvm_vm *vm);
 
+static inline uint32_t backing_src_guest_memfd_flags(enum vm_mem_backing_src_type t)
+{
+	uint32_t flags = GUEST_MEMFD_FLAG_MMAP;
+
+	if (t == VM_MEM_SRC_GUEST_MEMFD_NO_DIRECT_MAP)
+		flags |= GUEST_MEMFD_FLAG_NO_DIRECT_MAP;
+
+	return flags;
+}
+
 static inline int __vm_create_guest_memfd(struct kvm_vm *vm, uint64_t size,
 					uint64_t flags)
 {
