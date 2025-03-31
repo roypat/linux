eea2272b5a165f3053f7ab5d2cd09683d9e3039d
Nikita Kalyazin <kalyazin@amazon.com>
2025-03-31 10:15:35 +0000

    mm: userfaultfd: generic continue for non hugetlbfs
    Remove shmem-specific code from UFFDIO_CONTINUE implementation for
    non-huge pages by calling vm_ops->fault().  A new VMF flag,
    FAULT_FLAG_USERFAULT_CONTINUE, is introduced to avoid recursive call to
    handle_userfault().
    
    Suggested-by: James Houghton <jthoughton@google.com>
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -376,30 +376,47 @@ out:
 	return ret;
 }
 
-/* Handles UFFDIO_CONTINUE for all shmem VMAs (shared or private). */
+/* Handles UFFDIO_CONTINUE for all VMAs */
 static int mfill_atomic_pte_continue(pmd_t *dst_pmd,
 				     struct vm_area_struct *dst_vma,
 				     unsigned long dst_addr,
 				     uffd_flags_t flags)
 {
-	struct inode *inode = file_inode(dst_vma->vm_file);
-	pgoff_t pgoff = linear_page_index(dst_vma, dst_addr);
 	struct folio *folio;
 	struct page *page;
 	int ret;
+	struct vm_fault vmf = {
+		.vma = dst_vma,
+		.address = dst_addr,
+		.flags = FAULT_FLAG_WRITE | FAULT_FLAG_REMOTE |
+		    FAULT_FLAG_USERFAULT_CONTINUE,
+		.pte = NULL,
+		.page = NULL,
+		.pgoff = linear_page_index(dst_vma, dst_addr),
+	};
 
-	ret = shmem_get_folio(inode, pgoff, 0, &folio, SGP_NOALLOC);
-	/* Our caller expects us to return -EFAULT if we failed to find folio */
-	if (ret == -ENOENT)
-		ret = -EFAULT;
-	if (ret)
-		goto out;
-	if (!folio) {
+	if (!dst_vma->vm_ops || !dst_vma->vm_ops->fault)
+		return -EINVAL;
+
+retry:
+	ret = dst_vma->vm_ops->fault(&vmf);
+	if (ret & VM_FAULT_ERROR) {
 		ret = -EFAULT;
 		goto out;
 	}
 
-	page = folio_file_page(folio, pgoff);
+	if (ret & VM_FAULT_NOPAGE) {
+		ret = -EAGAIN;
+		goto out;
+	}
+
+	if (ret & VM_FAULT_RETRY)
+		goto retry;
+
+	page = vmf.page;
+	folio = page_folio(page);
+	BUG_ON(!folio);
+
 	if (PageHWPoison(page)) {
 		ret = -EIO;
 		goto out_release;
