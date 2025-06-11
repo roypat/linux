0d6ad36493831412e14582768d66363a60d52405
Ackerley Tng <ackerleytng@google.com>
2025-06-11 14:33:23 +0100

    KVM: x86: Consult guest_memfd when computing max_mapping_level
    This patch adds kvm_gmem_max_mapping_level(), which always returns
    PG_LEVEL_4K since guest_memfd only supports 4K pages for now.
    
    When guest_memfd supports shared memory, max_mapping_level (especially
    when recovering huge pages - see call to __kvm_mmu_max_mapping_level()
    from recover_huge_pages_range()) should take input from
    guest_memfd.
    
    Input from guest_memfd should be taken in these cases:
    
    + if the memslot supports shared memory (guest_memfd is used for
      shared memory, or in future both shared and private memory) or
    + if the memslot is only used for private memory and that gfn is
      private.
    
    If the memslot doesn't use guest_memfd, figure out the
    max_mapping_level using the host page tables like before.
    
    This patch also refactors and inlines the other call to
    __kvm_mmu_max_mapping_level().
    
    In kvm_mmu_hugepage_adjust(), guest_memfd's input is already
    provided (if applicable) in fault->max_level. Hence, there is no need
    to query guest_memfd.
    
    lpage_info is queried like before, and then if the fault is not from
    guest_memfd, adjust fault->req_level based on input from host page
    tables.
    
    Acked-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Co-developed-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2574,6 +2574,10 @@ static inline bool kvm_mem_is_private(struct kvm *kvm, gfn_t gfn)
 	return kvm_get_memory_attributes(kvm, gfn) & KVM_MEMORY_ATTRIBUTE_PRIVATE;
 }
 #else
+static inline unsigned long kvm_get_memory_attributes(struct kvm *kvm, gfn_t gfn)
+{
+	return 0;
+}
 static inline bool kvm_mem_is_private(struct kvm *kvm, gfn_t gfn)
 {
 	return false;
@@ -2584,6 +2588,7 @@ static inline bool kvm_mem_is_private(struct kvm *kvm, gfn_t gfn)
 int kvm_gmem_get_pfn(struct kvm *kvm, struct kvm_memory_slot *slot,
 		     gfn_t gfn, kvm_pfn_t *pfn, struct page **page,
 		     int *max_order);
+int kvm_gmem_mapping_order(const struct kvm_memory_slot *slot, gfn_t gfn);
 #else
 static inline int kvm_gmem_get_pfn(struct kvm *kvm,
 				   struct kvm_memory_slot *slot, gfn_t gfn,
@@ -2592,6 +2597,12 @@ static inline int kvm_gmem_get_pfn(struct kvm *kvm,
 {
 	KVM_BUG_ON(1, kvm);
 	return -EIO;
+}
+static inline int kvm_gmem_mapping_order(const struct kvm_memory_slot *slot,
+					 gfn_t gfn)
+{
+	BUILD_BUG();
+	return 0;
 }
 #endif /* CONFIG_KVM_GMEM */
 
